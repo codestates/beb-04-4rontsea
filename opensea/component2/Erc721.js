be68afbf34c371f57be3d6602a7b0cf4458a9cb3
namminwoo-hub <100527953+minwoogramer@users.noreply.github.com>
2022-06-14 13:12:04 +0900

    firstmerge

---
+++ opensea/component2/Erc721.js
@@ -0,0 +1,60 @@
+import erc721Abi from "./erc721Abi";
+
+function Erc721({ web3, account, erc721list }) {
+    const [to, setTo] = useState("");
+		const sendToken = async (tokenAddr, tokenId) => {
+            const tokenContract = await new web3.eth.Contract(
+                erc721Abi,
+                tokenAddr,
+                {
+                    from: account,
+                }
+            );
+            tokenContract.methods
+                .transferFrom(account, to, tokenId)
+                .send({
+                    from: account,
+                })
+                .on("receipt", (receipt) => {
+                    setTo("");
+                });
+         }
+		return (
+		  <div className="erc721list">
+		      {erc721list.map((token) => {
+		          return (
+		              <div className="erc721token">
+		                  Name: <span className="name">{token.name}</span>(
+		                  <span className="symbol">{token.symbol}</span>)
+		                  <div className="nft">id: {token.tokenId}</div>
+		                  <img src={token.tokenURI} width={300} />
+		                  
+											/* nft 전송 관련 */
+											<div className="tokenTransfer">
+		                      To:{" "}
+		                      <input
+		                          type="text"
+		                          value={to}
+		                          onChange={(e) => {
+		                              setTo(e.target.value);
+		                          }}
+		                      ></input>
+		                      <button
+		                          className="sendErc20Btn"
+		                          onClick={sendToken.bind(
+		                              this,
+		                              token.address,
+		                              token.tokenId
+		                          )}
+		                      >
+		                          send Token
+		                      </button>
+		                  </div>
+		              </div>
+		          );
+		      })}
+		  </div>
+		);
+}
+
+export default Erc721;
--- opensea/pages/index.js
+++ opensea/pages/index.js
@@ -1,10 +1,12 @@
 
 import { NavBar } from "../components/NavBar"
 import { Thumnail } from "../components/Thumnail"
+import { App } from "../components/App"
 export default function Home() {
   return (<>
     <NavBar/>
     <Thumnail/>
+    <App/>
     </>
   )
 }
