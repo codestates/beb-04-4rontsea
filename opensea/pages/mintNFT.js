72b62af7c358b97bfe171cdab74c027c1dcb0610
namminwoo-hub <100527953+minwoogramer@users.noreply.github.com>
2022-06-15 15:05:37 +0900

    upload

--- opensea/pages/mintNFT.js
+++ opensea/pages/mintNFT.js
@@ -1,5 +1,28 @@
 import { NavBar } from "../components/NavBar";
 
+
+function upload() {
+  const reader = new FileReader();
+  reader.onloadend = function() {
+    const ipfs = window.IpfsApi('localhost', 5001) // Connect to IPFS
+    const buf = buffer.Buffer(reader.result) // Convert data into buffer
+    ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+      if(err) {
+        console.error(err)
+        return
+      }
+      let url = `https://ipfs.io/ipfs/${result[0].hash}`
+      console.log(`Url --> ${url}`)
+      document.getElementById("url").innerHTML= url
+      document.getElementById("url").href= url
+      document.getElementById("output").src = url
+    })
+  }
+  const photo = document.getElementById("photo");
+  reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
+}
+
+
 export default function MintNFT() {
   return (
     <div>
@@ -7,10 +30,10 @@ export default function MintNFT() {
       <div>
         <h1>NFT 발행</h1>
         <form>
-          <div>
-            <label>파일업로드</label>
-            <input type="file" accept="image/*" require></input>
-          </div>
+          <fieldset>
+            <legend>파일업로드</legend>
+            <button onClick={upload()} id="photo" type="file" accept="image/*" require></button>
+          </fieldset>
           <div>
             <label>설명</label>
             <textarea required></textarea>
