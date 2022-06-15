046f149b2b9f0fab4ee48b04e987a961ef55658f
genius-jihye-park <sconenomad@gmail.com>
2022-06-15 17:44:08 +0900

    carousel styled component

--- opensea/components/Thumnail.js
+++ opensea/components/Thumnail.js
@@ -1,11 +1,39 @@
-import React, { Component } from "react";
+/**
+ * nom i react-responsive-carousel
+ * "react-responsive-carousel": "^3.2.20",
+ */
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+import { Carousel } from "react-responsive-carousel"; 
+import styled from "styled-components";
 
-export const Thumnail = () => {
+const CaroStyle = styled.div `
+.carousel .control-dots .dot
+{
+    background: #82C6E3;
+    box-shadow: 0px 4px 8px rgba(76, 166, 205, 0.2);
+    width:16px;
+    height:16px;
+}
+.carousel .control-dots .dot.selected,.carousel .control-dots .dot:hover
+{
+    opacity:1;filter:alpha(opacity=100);
+    width:18px;
+    height:18px;
+}
+`;
+
+export const Thumnail = ({ intervalMS,showArrows, description, fname, title, lname, avatarSrc,...props}) => {
   return (
-    <div>
-      <Carousel>
+    <div className="carousel">
+      {/* 사용법 : https://www.npmjs.com/package/react-responsive-carousel */}
+      <Carousel 
+       showArrows={showArrows}
+       infiniteLoop={true}
+       showThumbs={false}
+       showStatus={false}
+       autoPlay={true}
+       interval={intervalMS}//6100
+      >
         <div>
           <img src="https://nft-content.upbit.com/3c562a1c-6ef8-4227-9798-f1548e327a8e" />
           <p className="legend">압도하는 모노드로잉의 세계</p>
