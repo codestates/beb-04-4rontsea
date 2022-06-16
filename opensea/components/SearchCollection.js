b2126cb2f2dafc7365bc26b57ac930ed06178e71
minwoogramer <01082249606nmw@gmail.com>
2022-06-17 03:30:06 +0900

    쿼리 cryptopunk로 변경

--- opensea/components/SearchCollection.js
+++ opensea/components/SearchCollection.js
@@ -3,7 +3,7 @@ import { useMoralisWeb3Api } from "react-moralis";
 
 export const fetchSearchNFTs = async (Web3Api) => {
  
-  const options = { q: "opensea", chain: "eth", filter: "name" };
+  const options = { q: "cryptopunk", chain: "eth", filter: "name" };
   const NFTs = await Web3Api.token.searchNFTs(options);
   console.log(NFTs);
   return NFTs.result.slice(0,6);
